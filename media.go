a6c3611bf2adc3a2d35e9602a03ab3ed62baa6d4
Chris Joyce <chrisj@benon.com>
2018-10-20 10:11:18 +1000

    Media List
    Can now get list of all media

--- media.go
+++ media.go
@@ -1,17 +1,66 @@
 package goprowifi
 
-// MediaList ist of the contents of the SD card
-type MediaList struct {
-	ID    string `json:"id"`
-	Media []struct {
-		Device string `json:"d"`
-		Files  []struct {
-			Ls             string `json:"ls"`
-			EpochTimestamp string `json:"mod"`
-			FileName       string `json:"n"`
-			S              string `json:"s"`
-		} `json:"fs"`
-	} `json:"media"`
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Media list of the contents of the SD card
+type Media struct {
+	Device         string `json:"device"`
+	EpochTimestamp string `json:"epoch_timestamp"`
+	FileName       string `json:"file_name"`
+	MediaType      string `json:"media_type"`
+	Ls             string `json:"ls"`
+	S              string `json:"s"`
+}
+
+type cameraMediaList struct {
+	ID    string        `json:"id"`
+	Media []cameraMedia `json:"media"`
+}
+
+// cameraMedia ...
+type cameraMedia struct {
+	Device string  `json:"d"`
+	Files  []Files `json:"fs"`
+}
+
+// Files ...
+type Files struct {
+	Ls             string `json:"ls"`
+	EpochTimestamp string `json:"mod"`
+	FileName       string `json:"n"`
+	S              string `json:"s"`
+}
+
+// GetMediaList ist of the contents of the SD card
+//
+// http://10.5.5.9:8080/gp/gpMediaList
+func (g *Client) getMediaList() (mediaList []*Media, httpStatus int, err error) {
+
+	bodyBytes, httpStatus, err := g.request("http://10.5.5.9:8080/gp/gpMediaList")
+
+	reply := new(cameraMediaList)
+	err = json.Unmarshal(bodyBytes, &reply)
+	if err != nil {
+		err = fmt.Errorf("Can't unmarshal retrieve response %s", err)
+	}
+
+	for _, cm := range reply.Media {
+		for _, dm := range cm.Files {
+			nm := new(Media)
+			nm.Device = cm.Device
+			nm.EpochTimestamp = dm.EpochTimestamp
+			nm.FileName = dm.FileName
+			nm.Ls = dm.Ls
+			nm.S = dm.S
+			nm.MediaType = nm.FileName[len(nm.FileName)-3:]
+			mediaList = append(mediaList, nm)
+		}
+	}
+
+	return
 }
 
 // Media List
--- media_test.go
+++ media_test.go
@@ -1,1 +1,85 @@
 package goprowifi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_getMediaList(t *testing.T) {
+	type fields struct {
+		myDoer Doer
+	}
+
+	tests := []struct {
+		name           string
+		fields         fields
+		wantReply      []*Media
+		wantHTTPStatus int
+		wantErr        error
+	}{
+		{
+			name: "Hero5 OK",
+			fields: fields{
+				myDoer: testDoer{
+					responseCode: 200,
+					response: `{
+						"id": "3806895087272624399",
+						"media": [
+							{
+								"d": "100GOPRO",
+								"fs": [
+									{
+										"n": "GOPR1130.MP4",
+										"mod": "1539705058",
+										"ls": "11170499",
+										"s": "188100018"
+									},
+									{
+										"n": "GOPR1147.JPG",
+										"mod": "1539960090",
+										"s": "3843146"
+									}
+								]
+							}
+						]
+					}`,
+				},
+			},
+			wantReply: []*Media{
+				{
+					Device:         "100GOPRO",
+					EpochTimestamp: "1539705058",
+					FileName:       "GOPR1130.MP4",
+					MediaType:      "MP4",
+					Ls:             "11170499",
+					S:              "188100018",
+				},
+				{
+					Device:         "100GOPRO",
+					EpochTimestamp: "1539960090",
+					FileName:       "GOPR1147.JPG",
+					MediaType:      "JPG",
+					Ls:             "",
+					S:              "3843146",
+				},
+			},
+			wantHTTPStatus: 200,
+			wantErr:        nil,
+			// wantErr: errors.New("got false response"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			hero := CreateTestRestClient(tt.fields.myDoer)
+			gotReply, gotHTTPStatus, err := hero.getMediaList()
+
+			if err == nil {
+				assert.Equal(t, tt.wantHTTPStatus, gotHTTPStatus, "HTTPStatus did not match")
+				assert.Equal(t, tt.wantReply, gotReply, "Reply did not match")
+			}
+
+		})
+	}
+}
